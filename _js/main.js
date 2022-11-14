0f3965db41e5716e506a2b14d48aeb6eb684b4e3
Pedro Henrique Godri <pedrogodri@gmail.com>
2022-11-14 20:13:20 -0300

    Commit - Finalização do JS

--- _js/main.js
+++ _js/main.js
@@ -13,7 +13,7 @@ function buscarFilmes(filmePesquisa) {
             var filmes = response.data.Search;
             var mostrarFilmes = '';
 
-            for(var i = 0; i < filmes.length; i++) {
+            for (var i = 0; i < filmes.length; i++) {
                 mostrarFilmes += `
                     <div class="col-sm-6 col-md-4">
 			            <div class="thumbnail">
@@ -33,11 +33,46 @@ function buscarFilmes(filmePesquisa) {
 }
 
 function filmeDetalhes(id) {
-    sessionStorage.setItem('filmesID', id);
+    sessionStorage.setItem('filmeID', id);
     window.location = 'detalhes.html';
     return false;
 }
 
-function mostrarDetalhesFilme() {
+function mostraDetalhesFilme() {
+    var filmeID = sessionStorage.getItem('filmeID');
+    axios.get(`http://www.omdbapi.com/?i=${filmeID}&page=1&apikey=65d09b9d`)
+        .then(function (response) {
+            var filme = response.data;
+            console.log(filme);
+            var mostrarDetalhesFilme =
+            `
+                <div class="col-md-6">
+                    <img src="${filme.Poster}" class="img-responsive">
+                    <h3><strong>${filme.Title}</strong><h3>
+                </div>
+                <div class="col-md-6">
+                    <div class="well clearfix">
+                        <ul class="list-group">
+                            <li class="list-group-item"><strong>Gênero: </strong>${filme.Genre}</li>
+                            <li class="list-group-item"><strong>Lançamentos: </strong>${filme.Released}</li>
+                            <li class="list-group-item"><strong>Duração: </strong>${filme.Runtime}</li>
+                            <li class="list-group-item"><strong>Idiomas: </strong>${filme.Language}</li>
+                            <li class="list-group-item"><strong>Prêmios: </strong>${filme.Awards}</li>
+                            <li class="list-group-item"><strong>Atores: </strong>${filme.Actors}</li>
+                        </ul>
 
+                        <h3>Descrição</h3>
+                        ${filmes.Plot}
+                        <hr>
+                        <a href="http://imdb.com/title/${filme.imdbID}" target="_blank" class="btn btn-success pull-left">Ver no iMDB</a>
+                        <a href="index.html" class="btn btn-default pull-right">Voltar a Pesquisar</a>
+                    </div>
+                </div>
+            `;
+
+            document.getElementById('filmes').innerHTML = mostrarDetalhesFilme;
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 }
